86af853cd2d7f7740612e3a3502c733133c53505
Ruslan Denysyuk <hydra0@bigmir.net>
2020-02-27 15:25:42 +0200

    Rewrite the Notification component to be functional

--- client/components/Notification/index.js
+++ client/components/Notification/index.js
@@ -1,45 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import isArray from 'lodash/isArray';
 import map from 'lodash/map';
 import styles from './css/styles.css';
 
-class Notification extends Component {
-  static propTypes = {
-    className: PropTypes.string,
-    err: PropTypes.oneOfType([PropTypes.bool, PropTypes.string, PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.bool, PropTypes.string]))]),
-    text: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
-  }
+function Notification({ className, err, text = 'Success' }) {
+  return (
+    <div
+      className={cx('fade-in', className, styles.notification, {
+        err,
+        [styles.err]: err,
+      })}
+    >
 
-  static defaultProps = {
-    text: 'Success',
-  }
+      <div className={styles.icon} />
 
-  render() {
-    const { className, err, text } = this.props;
-
-    return (
-      <div
-        className={cx('fade-in', className, styles.notification, {
-          err,
-          [styles.err]: err,
-        })}
-      >
-
-        <div className={styles.icon} />
-
-        <div className={styles.text}>
-          {isArray(text) ? map(text, (item, key) => (
-            <div key={key}>
-              {item}
-            </div>
-          )) : text}
-        </div>
-
+      <div className={styles.text}>
+        {isArray(text) ? map(text, (item, key) => (
+          <div key={key}>
+            {item}
+          </div>
+        )) : text}
       </div>
-    );
-  }
+
+    </div>
+  );
 }
+
+Notification.propTypes = {
+  className: PropTypes.string,
+  err: PropTypes.oneOfType([PropTypes.bool, PropTypes.string, PropTypes.array]),
+  text: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
+};
 
 export default Notification;
